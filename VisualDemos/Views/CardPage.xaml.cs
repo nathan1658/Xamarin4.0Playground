70e3251cf8aac438332736ed41bc9c8c28c2266b
nathan1658 <pooikei165@gmail.com>
2019-08-16 23:52:08 +0800

    Initial commit

---
+++ VisualDemos/Views/CardPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace VisualDemos
+{
+    public partial class CardPage : ContentPage
+    {
+        public CardPage()
+        {
+            InitializeComponent();
+            var list = new List<object>
+            {
+            };
+            for (int i = 0; i < 50; i++)
+            {
+                list.Add(new object());
+            }
+            testListView.ItemsSource = list;
+            
+        }
+    }
+}
