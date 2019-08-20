97ac9bb47666bacf3fcebcca11e8cd9397e199c6
Nathan <>
2019-08-20 18:14:10 +0800

    Fix floating button

--- VisualDemos/Views/ConversationPage.xaml.cs
+++ VisualDemos/Views/ConversationPage.xaml.cs
@@ -8,12 +8,8 @@ namespace VisualDemos.Views
 {
     public partial class ConversationPage : ContentPage
     {
-        public ConversationPage()
-        {
-            InitializeComponent();
 
-            var htmlSource = new HtmlWebViewSource();
-            htmlSource.Html = @"<!DOCTYPE html>
+        string htmlString = @"<!DOCTYPE html>
 <html>
 <head>
 <style>
@@ -36,7 +32,6 @@ tr:nth-child(even) {
 </head>
 <body>
 
-<h2>HTML Table</h2>
 
 <table>
   <tr>
@@ -80,10 +75,26 @@ tr:nth-child(even) {
 </html>
 ";
 
+        public ConversationPage()
+        {
+            InitializeComponent();
+
+            var htmlSource = new HtmlWebViewSource();
+            htmlSource.Html = htmlString;
+
 
             webView.Source = htmlSource;
 
             ExpandButton.Clicked += ExpandButton_Clicked;
+            chatButton.Clicked += ChatButton_Clicked;
+        }
+
+        private void ChatButton_Clicked(object sender, EventArgs e)
+        {
+            var lbl = new Label { Text = "ChatUi" };
+            lbl.HorizontalOptions = lbl.VerticalOptions = LayoutOptions.Center;
+            var popUp = new PopUpFrame(lbl);
+            PopupNavigation.Instance.PushAsync(popUp);
         }
 
         private void ExpandButton_Clicked(object sender, EventArgs e)
@@ -91,72 +102,7 @@ tr:nth-child(even) {
 
             var tmpWebView = new WebView();
             var htmlSource = new HtmlWebViewSource();
-            htmlSource.Html = @"<!DOCTYPE html>
-<html>
-<head>
-<style>
-table {
-  font-family: arial, sans-serif;
-  border-collapse: collapse;
-  width: 100%;
-}
-
-td, th {
-  border: 1px solid #dddddd;
-  text-align: left;
-  padding: 8px;
-}
-
-tr:nth-child(even) {
-  background-color: #dddddd;
-}
-</style>
-</head>
-<body>
-
-<h2>HTML Table</h2>
-
-<table>
-  <tr>
-    <th>Company</th>
-    <th>Contact</th>
-    <th>Country</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>Maria Anders</td>
-    <td>Germany</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>Francisco Chang</td>
-    <td>Mexico</td>
-  </tr>
-  <tr>
-    <td>Ernst Handel</td>
-    <td>Roland Mendel</td>
-    <td>Austria</td>
-  </tr>
-  <tr>
-    <td>Island Trading</td>
-    <td>Helen Bennett</td>
-    <td>UK</td>
-  </tr>
-  <tr>
-    <td>Laughing Bacchus Winecellars</td>
-    <td>Yoshi Tannamuri</td>
-    <td>Canada</td>
-  </tr>
-  <tr>
-    <td>Magazzini Alimentari Riuniti</td>
-    <td>Giovanni Rovelli</td>
-    <td>Italy</td>
-  </tr>
-</table>
-
-</body>
-</html>
-";
+            htmlSource.Html = htmlString;
 
 
             tmpWebView.Source = htmlSource;
